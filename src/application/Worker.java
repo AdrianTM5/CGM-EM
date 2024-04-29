ae18a6e6f5e1f7dd2b6a4c93a2e80058f93b70f0
Adrian <angel220803@hotmail.com>
2024-04-29 17:11:39 -0600

    Se hicieron cambios, aún no se agrega la base de datos

--- src/application/Worker.java
+++ src/application/Worker.java
@@ -1,6 +1,31 @@
-package application;
-
-public class Worker 
-{
-	
-}
+//package application;
+//
+//public class Worker 
+//{
+//	private int id_trab, tipo_trab;
+//	private double sueld_trab;
+//	private String nom_trab, dep_trab, curp_trab, rfc_trab, col_trab, mun_trab, edoC_trab, sexo_trab, tel_trab, call_trab, ine_trab, gEst_trab, carr_trab, inst_trab;
+//	
+//	public Worker(int lastId, int tipo_trab, double sueld_trab, String nom_trab, String dep_trab, String curp_trab, String rfc_trab, String col_trab, String mun_trab, String edoC_trab, String sexo_trab, String tel_trab, String call_trab, String ine_trab, String gEst_trab, String carr_trab, String inst_trab) 
+//	{
+//		this.id_trab = lastId;
+//		this.tipo_trab = tipo_trab;
+//		this.sueld_trab = sueld_trab;
+//		this.nom_trab = nom_trab;
+//		this.dep_trab = dep_trab;
+//		this.curp_trab = curp_trab;
+//		this.rfc_trab = rfc_trab;
+//		this.col_trab = col_trab;
+//		this.mun_trab = mun_trab;
+//		this.edoC_trab = edoC_trab;
+//		this.sexo_trab = sexo_trab;
+//		this.tel_trab = tel_trab;
+//		this.call_trab = call_trab;
+//		this.ine_trab = ine_trab;
+//		this.gEst_trab = gEst_trab;
+//		this.carr_trab = carr_trab;
+//		this.inst_trab = inst_trab;
+//	}
+//	
+//	
+//}
